d81973f05bc0c6e373f8a024257701b09e159b25
Willow Ahrens <willow.ahrens@mit.edu>
2022-07-28 12:07:36 -0400

    whoops

--- pull_finch.sh
+++ pull_finch.sh
@@ -1,1 +1,1 @@
-git subtree pull --prefix Finch.jl git@github.com:willow-ahrens/Finch.jl.git main --squash
+git subtree pull --prefix Finch.jl git@github.com:willow-ahrens/Finch.jl.git master --squash
