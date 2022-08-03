a09258cc363040d52d528f7dd74fc8be09d05e79
Daniel Donenfeld <danieldonenfeld@gmail.com>
2022-08-03 14:59:14 -0400

    Use TensorDepot to download sketches and add repeat format test. There are probably still issues setting up the sparse/repeat matrices. Fix makefile and build scripts

--- build_opencv.sh
+++ build_opencv.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -z "$NPROC_VAL" ]
+then
+    NPROC_VAL=$(nproc)
+fi
+
 mkdir -p opencv/build
 mkdir -p opencv/install
 cd opencv/build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=../install -DBUILD_ZLIB=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF .. 
-make -j$(nproc)
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=../install -DBUILD_ZLIB=OFF -DBUILD_TESTS=OFF -DBUILD_PERF_TESTS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_opencv_apps=OFF -DBUILD_PNG=ON .. 
+make -j$NPROC_VAL
 make install
