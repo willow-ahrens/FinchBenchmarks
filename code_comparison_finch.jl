e5375824ee4151c75e51eb50a964e311cd9562bb
Willow Ahrens <willow.ahrens@mit.edu>
2022-08-13 16:22:19 -0400

    cleanup names

--- code_comparison_finch.jl
+++ code_comparison_finch.jl
@@ -5,20 +5,20 @@ using Finch.IndexNotation
 using SyntaxInterface
 using Finch: Run, Spike, Extent, Scalar, Cases, Stepper, Jumper, Step, Jump, AcceptRun, AcceptSpike, Thunk, Phase, Pipeline, Leaf, Simplify, Shift
 
-mutable struct SingleBlock{D, Tv, Ti} <: AbstractVector{Tv}
+mutable struct SparseBlock{D, Tv, Ti} <: AbstractVector{Tv}
     len::Ti
     start::Ti
     stop::Ti
     val::Vector{Tv}
 end
 
-function SingleBlock{D}(len::Ti, start::Ti, stop::Ti, val::Vector{Tv}) where {D, Ti, Tv}
-    SingleBlock{D, Tv, Ti}(len, start, stop, val)
+function SparseBlock{D}(len::Ti, start::Ti, stop::Ti, val::Vector{Tv}) where {D, Ti, Tv}
+    SparseBlock{D, Tv, Ti}(len, start, stop, val)
 end
 
-Base.size(vec::SingleBlock) = (vec.len,)
+Base.size(vec::SparseBlock) = (vec.len,)
 
-function Base.getindex(vec::SingleBlock{D, Tv, Ti}, i) where {D, Tv, Ti}
+function Base.getindex(vec::SparseBlock{D, Tv, Ti}, i) where {D, Tv, Ti}
     if i < vec.start
         D
     elseif i <= vec.stop
@@ -28,29 +28,29 @@ function Base.getindex(vec::SingleBlock{D, Tv, Ti}, i) where {D, Tv, Ti}
     end
 end
 
-mutable struct VirtualSingleBlock{Tv, Ti}
+mutable struct VirtualSparseBlock{Tv, Ti}
     ex
     name
     D
 end
 
-function Finch.virtualize(ex, ::Type{SingleBlock{D, Tv, Ti}}, ctx, tag=:tns) where {D, Tv, Ti}
+function Finch.virtualize(ex, ::Type{SparseBlock{D, Tv, Ti}}, ctx, tag=:tns) where {D, Tv, Ti}
     sym = ctx.freshen(tag)
     push!(ctx.preamble, :($sym = $ex))
-    VirtualSingleBlock{Tv, Ti}(sym, tag, D)
+    VirtualSparseBlock{Tv, Ti}(sym, tag, D)
 end
 
-(ctx::Finch.LowerJulia)(tns::VirtualSingleBlock) = tns.ex
+(ctx::Finch.LowerJulia)(tns::VirtualSparseBlock) = tns.ex
 
-function Finch.getdims(arr::VirtualSingleBlock{Tv, Ti}, ctx::Finch.LowerJulia, mode) where {Tv, Ti}
+function Finch.getdims(arr::VirtualSparseBlock{Tv, Ti}, ctx::Finch.LowerJulia, mode) where {Tv, Ti}
     ex = Symbol(arr.name, :_stop)
     push!(ctx.preamble, :($ex = $size($(arr.ex))[1]))
     (Extent(1, Virtual{Ti}(ex)),)
 end
-Finch.setdims!(arr::VirtualSingleBlock{Tv, Ti}, ctx::Finch.LowerJulia, mode, dims...) where {Tv, Ti} = arr
-Finch.getname(arr::VirtualSingleBlock) = arr.name
-Finch.setname(arr::VirtualSingleBlock, name) = (arr_2 = deepcopy(arr); arr_2.name = name; arr_2)
-function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSingleBlock})
+Finch.setdims!(arr::VirtualSparseBlock{Tv, Ti}, ctx::Finch.LowerJulia, mode, dims...) where {Tv, Ti} = arr
+Finch.getname(arr::VirtualSparseBlock) = arr.name
+Finch.setname(arr::VirtualSparseBlock, name) = (arr_2 = deepcopy(arr); arr_2.name = name; arr_2)
+function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSparseBlock})
     if ctx.root isa Loop && ctx.root.idx == Finch.get_furl_root(node.idxs[1])
         Finch.ChunkStyle()
     else
@@ -58,7 +58,7 @@ function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSingleBlock})
     end
 end
 
-function (ctx::Finch.ChunkifyVisitor)(node::Access{VirtualSingleBlock{Tv, Ti}, Read}, ::Finch.DefaultStyle) where {Tv, Ti}
+function (ctx::Finch.ChunkifyVisitor)(node::Access{VirtualSparseBlock{Tv, Ti}, Read}, ::Finch.DefaultStyle) where {Tv, Ti}
     vec = node.tns
     if getname(ctx.idx) == getname(node.idxs[1])
         tns = Pipeline([
@@ -80,48 +80,48 @@ function (ctx::Finch.ChunkifyVisitor)(node::Access{VirtualSingleBlock{Tv, Ti}, R
     end
 end
 
-mutable struct SimpleSparseVector{D, Tv, Ti} <: AbstractVector{Tv}
+mutable struct SparseList{D, Tv, Ti} <: AbstractVector{Tv}
     len::Ti
     idx::Vector{Ti}
     val::Vector{Tv}
 end
 
-function SimpleSparseVector{D}(idx::Vector{Ti}, val::Vector{Tv}) where {D, Ti, Tv}
-    SimpleSparseVector{D, Tv, Ti}(idx, val)
+function SparseList{D}(idx::Vector{Ti}, val::Vector{Tv}) where {D, Ti, Tv}
+    SparseList{D, Tv, Ti}(idx, val)
 end
 
-Base.size(vec::SimpleSparseVector) = (vec.len,)
+Base.size(vec::SparseList) = (vec.len,)
 
-function Base.getindex(vec::SimpleSparseVector{D, Tv, Ti}, i) where {D, Tv, Ti}
+function Base.getindex(vec::SparseList{D, Tv, Ti}, i) where {D, Tv, Ti}
     p = findfirst(j->j >= i, vec.idx)
     vec.idx[p] == i ? vec.val[p] : D
 end
 
-mutable struct VirtualSimpleSparseVector{Tv, Ti}
+mutable struct VirtualSparseList{Tv, Ti}
     ex
     name
     D
 end
 
-Finch.default(vec::VirtualSimpleSparseVector) = vec.D
+Finch.default(vec::VirtualSparseList) = vec.D
 
-function Finch.virtualize(ex, ::Type{SimpleSparseVector{D, Tv, Ti}}, ctx, tag=:tns) where {D, Tv, Ti}
+function Finch.virtualize(ex, ::Type{SparseList{D, Tv, Ti}}, ctx, tag=:tns) where {D, Tv, Ti}
     sym = ctx.freshen(tag)
     push!(ctx.preamble, :($sym = $ex))
-    VirtualSimpleSparseVector{Tv, Ti}(sym, tag, D)
+    VirtualSparseList{Tv, Ti}(sym, tag, D)
 end
 
-(ctx::Finch.LowerJulia)(tns::VirtualSimpleSparseVector) = tns.ex
+(ctx::Finch.LowerJulia)(tns::VirtualSparseList) = tns.ex
 
-function Finch.getdims(arr::VirtualSimpleSparseVector{Tv, Ti}, ctx::Finch.LowerJulia, mode) where {Tv, Ti}
+function Finch.getdims(arr::VirtualSparseList{Tv, Ti}, ctx::Finch.LowerJulia, mode) where {Tv, Ti}
     ex = Symbol(arr.name, :_stop)
     push!(ctx.preamble, :($ex = $size($(arr.ex))[1]))
     (Extent(1, Virtual{Ti}(ex)),)
 end
-Finch.setdims!(arr::VirtualSimpleSparseVector{Tv, Ti}, ctx::Finch.LowerJulia, mode, dims...) where {Tv, Ti} = arr
-Finch.getname(arr::VirtualSimpleSparseVector) = arr.name
-Finch.setname(arr::VirtualSimpleSparseVector, name) = (arr_2 = deepcopy(arr); arr_2.name = name; arr_2)
-function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSimpleSparseVector})
+Finch.setdims!(arr::VirtualSparseList{Tv, Ti}, ctx::Finch.LowerJulia, mode, dims...) where {Tv, Ti} = arr
+Finch.getname(arr::VirtualSparseList) = arr.name
+Finch.setname(arr::VirtualSparseList, name) = (arr_2 = deepcopy(arr); arr_2.name = name; arr_2)
+function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSparseList})
     if ctx.root isa Loop && ctx.root.idx == Finch.get_furl_root(node.idxs[1])
         Finch.ChunkStyle()
     else
@@ -129,7 +129,7 @@ function (ctx::Finch.Stylize{LowerJulia})(node::Access{<:VirtualSimpleSparseVect
     end
 end
 
-function (ctx::Finch.ChunkifyVisitor)(node::Access{VirtualSimpleSparseVector{Tv, Ti}, Read}, ::Finch.DefaultStyle) where {Tv, Ti}
+function (ctx::Finch.ChunkifyVisitor)(node::Access{VirtualSparseList{Tv, Ti}, Read}, ::Finch.DefaultStyle) where {Tv, Ti}
     vec = node.tns
     my_i = ctx.ctx.freshen(getname(vec), :_i0)
     my_i′ = ctx.ctx.freshen(getname(vec), :_i1)
@@ -175,10 +175,8 @@ end
 
 Finch.register()
 
-
-
-A = SimpleSparseVector{0.0, Float64, Int}(10, [1, 3, 5, 7, 9, 11], [2.0, 3.0, 4.0, 5.0, 6.0])
-B = SingleBlock{0.0, Float64, Int}(10, 3, 9, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
+A = SparseList{0.0, Float64, Int}(10, [1, 3, 5, 7, 9, 11], [2.0, 3.0, 4.0, 5.0, 6.0])
+B = SparseBlock{0.0, Float64, Int}(10, 3, 9, [1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0])
 C = Scalar{0.0}()
 
 println(@index_code @loop i C[] += A[i] * B[i])
