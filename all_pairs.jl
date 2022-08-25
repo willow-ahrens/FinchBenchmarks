16502b285cee95b3a17f32c94a8eeb20a6f3d54d
Willow Ahrens <willow.ahrens@mit.edu>
2022-08-25 16:25:00 -0400

    cool

--- all_pairs.jl
+++ all_pairs.jl
@@ -61,16 +61,14 @@ Finch.register()
 
 function all_pairs_finch_kernel(m, T, O)
     o = Scalar{0.0}()
-    @finch @loop k l @sieve m[k,l] ((O[k,l] = sqrt(o[])) where (@loop i j o[] += sq(convert($(value(Float64)),T[k, i, j]) - convert($(value(Float64)),T[l, i, j]))))
-    #o = Scalar{0}()
-    #S = @fiber(s(e(0.0)))
-    #@finch @loop k i j S[k] += sq(convert($(value(Int)), T[k, i, j]))
-    #@finch @loop k l @sieve m[k,l] ((O[k,l] = sqrt(S[k] + S[l] - 2 * o[])) where (@loop i j o[] += convert($(value(Float64)),T[k, i, j]) * convert($(value(Float64)),T[l, i, j])))
+    @finch @loop k l @sieve m[k,l] ((O[k,l] = sqrt(o[])) where (@loop ij o[] += $sq(convert($(value(Float64)),T[k, ij]) - convert($(value(Float64)),T[l, ij]))))
 end
 
 function all_pairs_finch(tensor_func, num_imgs)
     mnist_arr = (tensor_func(1:num_imgs))
-    T = dropdefaults!(@fiber(d(d(sl(e($(0x0::UInt8)))))),copy(rawview(channelview(mnist_arr))))
+    T = dropdefaults!(@fiber(d(sl(e($(0x00::UInt8))))),reshape(copy(rawview(channelview(mnist_arr))), num_imgs, :))
+    println(length(copy(rawview(channelview(mnist_arr)))))
+    println(length(T.lvl.lvl.lvl.val))
     O = fiber(zeros(Float64,num_imgs,num_imgs))
     
     dense_m = [i < j for i in 1:num_imgs, j in 1:num_imgs]
@@ -81,22 +79,29 @@ function all_pairs_finch(tensor_func, num_imgs)
     return finch_time, O
 end
 
-function all_pairs_finch_rle_kernel(m, T, O)
-    o = Scalar{0.0}()
-    println(@finch_code @loop k l @sieve m[k,l] ((O[k,l] = sqrt(o[])) where (@loop i j o[] += sq(convert($(value(Float64)),T[k, i, j]) - convert($(value(Float64)),T[l, i, j])))))
-    exit()
-end
-
-function all_pairs_finch_rle(tensor_func, num_imgs)
+function all_pairs_finch_vbl(tensor_func, num_imgs)
     mnist_arr = (tensor_func(1:num_imgs))
-    T = copyto!(@fiber(d(d(sl(r($(0x0::UInt8)))))),copy(rawview(channelview(mnist_arr))))
-    println(length(T.lvl.lvl.lvl.lvl.val))
+    T = dropdefaults!(@fiber(d(sv(e($(0x00::UInt8))))),reshape(copy(rawview(channelview(mnist_arr))), num_imgs, :))
     O = fiber(zeros(Float64,num_imgs,num_imgs))
     
     dense_m = [i < j for i in 1:num_imgs, j in 1:num_imgs]
     m = dropdefaults!(@fiber(d(sl(p()))), dense_m)
 
-    finch_time = @belapsed all_pairs_finch_rle_kernel($m, $T, $O)
+    finch_time = @belapsed all_pairs_finch_kernel($m, $T, $O)
+
+    return finch_time, O
+end
+
+function all_pairs_finch_rle(tensor_func, num_imgs)
+    mnist_arr = (tensor_func(1:num_imgs))
+    T = copyto!(@fiber(d(rl($(0x00::UInt8)))),reshape(copy(rawview(channelview(mnist_arr))), num_imgs, :))
+    println(length(T.lvl.lvl.val))
+    O = fiber(zeros(Float64,num_imgs,num_imgs))
+    
+    dense_m = [i < j for i in 1:num_imgs, j in 1:num_imgs]
+    m = dropdefaults!(@fiber(d(sl(p()))), dense_m)
+
+    finch_time = @belapsed all_pairs_finch_kernel($m, $T, $O)
 
     return finch_time, O
 end
@@ -107,6 +112,7 @@ function all_pairs_opencv(tensor_func, num_imgs, result_compare)
     for i in 1:num_imgs
         img = img_to_dense(T[i, :, :])
         pngwrite(joinpath(tmp_tensor_dir, "$i.png"), ffindnz(img)..., size(img))
+        pngwrite("$i.png", ffindnz(img)..., size(img))
     end
 
     io = IOBuffer()
@@ -120,27 +126,20 @@ function all_pairs_opencv(tensor_func, num_imgs, result_compare)
 
     result = fsparse(ttread(joinpath(tmp_tensor_dir, "result.ttx"))...)
 
-    comp =  FiberArray(result_compare)
-    res = FiberArray(result)
-    # display(comp)
-    # println()    
-    # display(res)
-    # println()
-    # @assert comp == res
-    #TODO: Reenable assertion, currently erroring: "ERROR: LoadError: AssertionError: comp == res"
-
-    return opencv_time
+    return (opencv_time, result)
 end
 
 run(pipeline(`make all_pairs_opencv`))
 
+
 dataset = mnist
-num_imgs = 5
+num_imgs = 20 
 
 finch_time, result = all_pairs_finch(dataset, num_imgs)
-#finch_rle_time, result = all_pairs_finch_rle(dataset, num_imgs)
-opencv_time = all_pairs_opencv(dataset, num_imgs, result)
+finch_vbl_time, result = all_pairs_finch_vbl(dataset, num_imgs)
+finch_rle_time, result = all_pairs_finch_rle(dataset, num_imgs)
+opencv_time, result = all_pairs_opencv(dataset, num_imgs, result)
 
-println("Finch (sparse) time : ", finch_time, " -- ", opencv_time/finch_time, "x faster than OpenCV")
+println("Finch time : ", finch_time, " -- ", opencv_time/finch_time, "x faster than OpenCV")
+println("Finch (vbl) time : ", finch_vbl_time, " -- ", opencv_time/finch_vbl_time, "x faster than OpenCV")
 println("Finch (rle) time : ", finch_rle_time, " -- ", opencv_time/finch_rle_time, "x faster than OpenCV")
-println("OpenCV time         : ", opencv_time)
--- spmv.jl
+++ spmv.jl
@@ -12,22 +12,27 @@ function spmv_taco(A, x)
     @finch @loop i j y_ref[i] += A[i, j] * x[j]
     @finch @loop i y_ref[i] = 0
 
-    @info :y
-    ttwrite("y.ttx", ffindnz(y_ref)..., size(y_ref))
-    (I, J, V) = findnz(A)
-    @info :A
-    ttwrite("A.ttx", (I, J), V, size(A))
-    @info :x
-    ttwrite("x.ttx", ffindnz(x)..., size(x))
+    y_file = joinpath(mktempdir(prefix="spmv_taco_$(key)"), "y.ttx")
+    persist_dir = joinpath(get_scratch!("Finch-CGO-2023"), "spmv_taco_$(key)")
+    mkpath(persist_dir)
+    A_file = joinpath(persist_dir, "A.ttx")
+    x_file = joinpath(persist_dir, "x.ttx")
+
+    ttwrite(y_file, (), [0], ())
+    if !(isfile(A_file) && isfile(x_file))
+        (I, J, V) = findnz(A)
+        ttwrite(A_file, (I, J), ones(Int32, length(V)), size(A))
+        ttwrite("x.ttx", ffindnz(x)..., size(x))
+    end
 
     io = IOBuffer()
 
     @info :run
-    run(pipeline(`./spmv_taco y.ttx A.ttx x.ttx`, stdout=io))
+    run(pipeline(`./spmv_taco $y_file $A_file $x_file`, stdout=io))
 
-    y = fsparse(ttread("y.ttx")...)
+    #y = fsparse(ttread(y_file)...)
 
-    @finch @loop i j y_ref[i] += A[i, j] * x[j]
+    #@finch @loop i j y_ref[i] += A[i, j] * x[j]
 
     #@assert FiberArray(y) == FiberArray(y_ref)
 
@@ -42,92 +47,21 @@ function spmv_finch(_A, x)
     return @belapsed (A = $A; x = $x; y = $y; @finch @loop i j y[i] += A[i, j] * x[j])
 end
 
-foo(y, A, x) = @inbounds begin
-    y_lvl = y.lvl
-    y_lvl_2 = y_lvl.lvl
-    y_lvl_2_val_alloc = length(y_lvl.lvl.val)
-    y_lvl_2_val = 0.0
-    A_lvl = A.lvl
-    A_lvl_2 = A_lvl.lvl
-    A_lvl_2_pos_alloc = length(A_lvl_2.pos)
-    A_lvl_2_idx_alloc = length(A_lvl_2.idx)
-    A_lvl_3 = A_lvl_2.lvl
-    A_lvl_3_val_alloc = length(A_lvl_2.lvl.val)
-    A_lvl_3_val = 0.0
-    x_lvl = x.lvl
-    x_lvl_2 = x_lvl.lvl
-    x_lvl_2_val_alloc = length(x_lvl.lvl.val)
-    x_lvl_2_val = 0.0
-    j_stop = A_lvl_2.I
-    i_stop = A_lvl.I
-    y_lvl_2_val_alloc = (Finch).refill!(y_lvl_2.val, 0.0, 0, 4)
-    y_lvl_2_val_alloc < 1 * A_lvl.I && (y_lvl_2_val_alloc = (Finch).refill!(y_lvl_2.val, 0.0, y_lvl_2_val_alloc, 1 * A_lvl.I))
-    for i = 1:i_stop
-        y_lvl_q = (1 - 1) * A_lvl.I + i
-        A_lvl_q = (1 - 1) * A_lvl.I + i
-        y_lvl_2_val = y_lvl_2.val[y_lvl_q]
-        A_lvl_2_q = A_lvl_2.pos[A_lvl_q]
-        A_lvl_2_q_stop = A_lvl_2.pos[A_lvl_q + 1]
-        if A_lvl_2_q < A_lvl_2_q_stop
-            A_lvl_2_i = A_lvl_2.idx[A_lvl_2_q]
-            A_lvl_2_i1 = A_lvl_2.idx[A_lvl_2_q_stop - 1]
-        else
-            A_lvl_2_i = 1
-            A_lvl_2_i1 = 0
-        end
-        j = 1
-        j_start = j
-        phase_start = max(j_start)
-        phase_stop = min(A_lvl_2_i1, j_stop)
-        if phase_stop >= phase_start
-            j = j
-            j = phase_start
-            while A_lvl_2_q < A_lvl_2_q_stop && A_lvl_2.idx[A_lvl_2_q] < phase_start
-                A_lvl_2_q += 1
-            end
-            while j <= phase_stop
-                j_start_2 = j
-                A_lvl_2_i = A_lvl_2.idx[A_lvl_2_q]
-                phase_stop_2 = min(A_lvl_2_i, phase_stop)
-                j_2 = j
-                if A_lvl_2_i == phase_stop_2
-                    A_lvl_3_val = A_lvl_3.val[A_lvl_2_q]
-                    j_3 = phase_stop_2
-                    x_lvl_q = (1 - 1) * x_lvl.I + j_3
-                    x_lvl_2_val = x_lvl_2.val[x_lvl_q]
-                    y_lvl_2_val = y_lvl_2_val + A_lvl_3_val * x_lvl_2_val
-                    A_lvl_2_q += 1
-                else
-                end
-                j = phase_stop_2 + 1
-            end
-            j = phase_stop + 1
-        end
-        j_start = j
-        phase_start_3 = max(j_start)
-        phase_stop_3 = min(j_stop)
-        if phase_stop_3 >= phase_start_3
-            j_4 = j
-            j = phase_stop_3 + 1
-        end
-        y_lvl_2.val[y_lvl_q] = y_lvl_2_val
-    end
-    (y = Fiber((Finch.DenseLevel){Int64}(A_lvl.I, y_lvl_2), (Finch.Environment)(; name = :y)),)
-end
-
-function spmv_foo(_A, x)
-    A = fiber(_A)
+function spmv_finch_vbl(_A, x)
+    A = copyto!(@fiber(d(sv(e(0.0)))), fiber(_A))
     y = fiber(x)
-    x = fiber(copy(x))
-    return @belapsed foo($y, $A, $x)
+    x = fiber(x)
+    println(@finch_code @loop i j y[i] += A[i, j] * x[j])
+    return @belapsed (A = $A; x = $x; y = $y; @finch @loop i j y[i] += A[i, j] * x[j])
 end
-
 
 @info "loading"
+
 A = SparseMatrixCSC(matrixdepot("Boeing/ct20stif"))
 (m, n) = size(A)
-#@info "taco"
-#println("taco_time: ", spmv_taco(A, rand(n)))
-#@info "finch"
-#println("finch_time: ", spmv_finch(A, rand(n)))
-println("finch_time: ", spmv_foo(A, rand(n)))
+@info "taco"
+println("taco_time: ", spmv_taco(A, rand(n)))
+@info "finch"
+println("finch_time: ", spmv_finch(A, rand(n)))
+@info "finch_vbl"
+println("finch_vbl_time: ", spmv_finch_vbl(A, rand(n)))
