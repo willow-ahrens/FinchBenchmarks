7677782fbac79b7f7408280dbc70ddfb876510ca
Willow Ahrens <willow.ahrens@mit.edu>
2022-08-29 13:33:45 -0400

    bigger alpha

--- conv_opencv.cpp
+++ conv_opencv.cpp
@@ -2,6 +2,7 @@
 #include <opencv2/core/mat.hpp>
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
 #include <iostream>
 #include <fstream>
 #include "benchmark.hpp"
@@ -32,29 +33,29 @@ void mtxWriteDense(std::string filename, cv::Mat& m){
 int main(int argc, char **argv)
 {
     cv::setNumThreads(1);
-    if(argc != 4){
+    if(argc != 3){
         std::cerr << "wrong number of arguments " << argc << std::endl;
     }
 
     std::string A_path = argv[1];
-    std::string result_path = argv[3];
+    std::string C_path = argv[2];
 
     Mat A = imread(A_path, IMREAD_GRAYSCALE);
-    Mat C(1000,1000, CV_U8, 0);
-    Mat F(11,11, CV_U8, 1);
+    Mat C(1000,1000, CV_8U, 0);
+    Mat F = Mat::ones(11,11, CV_64F);
 
     // Assemble output indices and numerically compute the result
     auto time = benchmark(
         []() {},
         [&]() {
-            filter2D(A, C, -1, F)
+            cv::filter2D(A, C, -1, F);
         }
     );
 
 
     std::cout << time << std::endl;
     
-    mtxWriteDense(C_file, C);
+    mtxWriteDense(C_path, C);
 
     return 0;
 }
