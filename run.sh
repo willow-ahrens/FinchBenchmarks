6b5458213e9dc2a946b81f56f6c7c231bdc6231c
Daniel Donenfeld <dbd64@cornell.edu>
2022-11-22 16:53:10 -0500

    Minor script updates

--- run.sh
+++ run.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
-#SBATCH -N 1
-#SBATCH --exclusive
-#SBATCH -t 240:00:00
-#SBATCH -t 4-0
-#SBATCH -e slurm-%A_%a.err
-#SBATCH -o slurm-%A_%a.out
-#SBATCH --partition=lanka-v3
 
-bash -e spmspv.sl
-bash -e triangle.sl
-bash -e conv.sl
-bash -e alpha.sl 
-bash -e all_pairs.sl
+bash -e spmspv.sh
+bash -e triangle.sh
+bash -e conv.sh
+bash -e alpha.sh
+bash -e all_pairs.sh
