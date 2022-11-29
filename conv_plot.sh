a718711a1274defeced04224443b0be4b9926b79
Daniel Donenfeld <danieldonenfeld@gmail.com>
2022-11-28 19:59:34 -0500

    Update plotting scripts

--- conv_plot.sh
+++ conv_plot.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 
+export SCRATCH=$(pwd)/scratch
+mkdir -p $SCRATCH
+export MATRIXDEPOT_DATA=$SCRATCH/MatrixData
+export TENSORDEPOT_DATA=$SCRATCH/TensorData
+export DATADEPS_ALWAYS_ACCEPT=true
+export DATADEPS_NO_STANDARD_LOAD_PATH=true
+export DATADEPS_DISABLE_ERROR_CLEANUP=true
+
+if [[ -f "$SCRIPT_DIR/download_julia.sh" ]]; then
+    bash -e $SCRIPT_DIR/download_julia.sh
+    source julia_env.sh
+fi
+
 julia --project=. conv_plot.jl conv_results.json conv_plot.png
