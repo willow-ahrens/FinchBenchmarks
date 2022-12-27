3f2eb10b3fc60bf6c51758a3d786957fbe698605
Willow Ahrens <ptrahrens@gmail.com>
2022-12-27 12:12:56 -0500

    int32 conv

--- julia_env.sh
+++ julia_env.sh
@@ -1,3 +1,3 @@
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(pwd)
 export JULIA_DEPOT_PATH=$SCRIPT_DIR/.julia
 export PATH=$PATH:$SCRIPT_DIR/julia-1.8.2/bin
