e1aecd7faca9a6ce3871813f970879406716bb19
teocollin1995 <teoc@mit.edu>
2024-03-04 21:32:15 -0500

    initial histogram works

--- hist/hist.jl
+++ hist/hist.jl
@@ -1,4 +1,4 @@
-# using Finch
+using Finch
 using TestImages
 using ImageCore, OpenCV, TestImages, MosaicViews
 using BenchmarkTools
@@ -7,12 +7,48 @@ using ArgParse
 using DataStructures
 
 
+function finch_hist_serial_kernel(result, img, mask)
+    @finch begin
+        result .= 0 
+        for x=_
+            for y=_
+                if mask[1, x,y] > UInt8(0)
+                    result[div(img[1, x,y], 16) + 1] += 1
+                end
+            end
+        end
+    end
+    return result
+end
 
-function opencv_blur(img, nthr)
+
+function finch_hist_run(f, img, nthr)
     #step -1: set num Threads
     OpenCV.setNumThreads(nthr)
     # step 0: setup results
-    results = Vector{Int32}([0 for _ in 1:256])
+    results = Vector{Float32}([0 for _ in 1:16])
+    # step 1: gray scale the image
+    # vector of images - don't ask
+    imgs = Vector{OpenCV.InputArray}([])
+    img_raw =  collect(rawview(channelview(img)))
+    img_gray = OpenCV.cvtColor(img_raw, OpenCV.COLOR_RGB2GRAY)
+    push!(imgs, img_gray)
+    mask = ones(UInt8, size(img_gray))
+    result = Ref{Any}()
+    channels = Vector{Int32}([0])
+    bins = Vector{Int32}([16])
+    regions = Vector{Float32}([0.0, 256.0])
+    time = @belapsed $result[] = $f($results, $img_gray, $mask)
+    (;time=time, result=result[])
+end
+
+finch_hist_serial(img, nthr) = finch_hist_run(finch_hist_serial_kernel, img, nthr)
+
+function opencv_hist(img, nthr)
+    #step -1: set num Threads
+    OpenCV.setNumThreads(nthr)
+    # step 0: setup results
+    results = Vector{Float32}([0 for _ in 1:16])
     # step 1: gray scale the image
     # vector of images - don't ask
     imgs = Vector{OpenCV.InputArray}([])
@@ -25,7 +61,7 @@ function opencv_blur(img, nthr)
     bins = Vector{Int32}([16])
     regions = Vector{Float32}([0.0, 256.0])
     time = @belapsed $result[] = OpenCV.calcHist($imgs, $channels, OpenCV.Mat($mask), $bins, $regions)
-    (;time=time, result=result[])
+    (;time=time, result=reshape(Array(result[]), 16))
 
 end
 
@@ -66,7 +102,8 @@ for img in datasets[parsed_args["dataset"]]
     imgLoad = testimage(img)
     hist_ref = nothing
     for (key, method) in [
-        "opencv" => opencv_blur
+        "opencv" => opencv_hist,
+        "finch_hist_serial" => finch_hist_serial
     ] 
         @info "testing" key img
         res = method(imgLoad, num_threads)
