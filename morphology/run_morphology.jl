24ad56071d8beabb5473c853183a8566d5b0725b
Willow Ahrens <willow.marie.ahrens@gmail.com>
2024-03-15 17:57:42 -0400

    fix

--- morphology/run_morphology.jl
+++ morphology/run_morphology.jl
@@ -13,6 +13,7 @@ using Serialization
 using OpenCV#, TestImages, MosaicViews, Colors, Images, FileIO
 using BenchmarkTools
 using LinearAlgebra
+using ArgParse
 using JSON
 using Base: summarysize
 
@@ -77,96 +78,127 @@ function magnify(img, factor)
     img = reshape(img, m * factor, n * factor)
 end
 
-function main(resultfile)
-    OpenCV.setNumThreads(1)
+s = ArgParseSettings("Run graph experiments.")
 
-    results = []
-    N = 4
+@add_arg_table! s begin
+    "--output", "-o"
+        arg_type = String
+        help = "output file path"
+        default = "morphology_results.json"
+    "--dataset", "-d"
+        arg_type = String
+        help = "dataset keyword"
+        default = "all"
+    "--num_trials", "-t"
+        arg_type = Int
+        help = "how many images to run"
+        default = 4
+end
 
-    sample(r) = r[randperm(end)[1:min(N, end)]]
+parsed_args = parse_args(ARGS, s)
+println(parsed_args)
 
-    mnist_i = sample(1:mnist_train_length())
-    omniglot_i = sample(1:omniglot_train_length())
-    humansketches_i = sample(1:humansketches_length())
-    testimage_dip3e_i = sample(dip3e_masks)
+OpenCV.setNumThreads(1)
 
-    for (dataset, getdata, I, f) in [
-        ("mnist", mnist_train, mnist_i, (img) -> Array{UInt8}(img .> 0x02)),
-        ("omniglot", omniglot_train, omniglot_i, (img) -> Array{UInt8}(img .== 0x00)),
-        ("humansketches", humansketches, humansketches_i, (img) -> Array{UInt8}(reinterpret(UInt8, img) .< 0xF0)),
-        ("testimage_dip3e", testimage_dip3e, testimage_dip3e_i, (img) -> Array{UInt8}(Array{Gray}(img) .> 0.1)),
-        ("mnist_magnify", mnist_train, mnist_i, (img) -> Array{UInt8}(magnify(img, MAG_FACTOR) .> 0x02)),
-        ("omniglot_magnify", omniglot_train, omniglot_i, (img) -> Array{UInt8}(magnify(img, MAG_FACTOR) .== 0x00)),
-        ("humansketches_magnify", humansketches, humansketches_i, (img) -> Array{UInt8}(magnify(reinterpret(UInt8, img), MAG_FACTOR) .< 0xF0)),
-        ("testimage_dip3e_magnify", testimage_dip3e, testimage_dip3e_i, (img) -> Array{UInt8}(magnify(Array{Gray}(img), MAG_FACTOR) .> 0.1)),
-    ]
-        for i in I
-            input = f(getdata(i))
+results = []
+N = parsed_args["num_trials"]
 
-            for (op, prep, kernels) in [
-                ("fill", prep_fill, [
-                    (method = "opencv", fn = fill_opencv),
-                    (method = "finch", fn = fill_finch),
-                    (method = "finch_scatter", fn = fill_finch_scatter),
-                ]),
-                ("erode2", (img) -> (img, 2), [
-                    (method = "opencv", fn = erode_opencv),
-                    (method = "finch", fn = erode_finch),
-                    (method = "finch_rle", fn = erode_finch_rle),
-                    (method = "finch_bits", fn = erode_finch_bits),
-                    (method = "finch_bits_mask", fn = erode_finch_bits_mask),
-                ]),
-                ("erode4", (img) -> (img, 4), [
-                    (method = "opencv", fn = erode_opencv),
-                    (method = "finch", fn = erode_finch),
-                    (method = "finch_rle", fn = erode_finch_rle),
-                    (method = "finch_bits", fn = erode_finch_bits),
-                    (method = "finch_bits_mask", fn = erode_finch_bits_mask),
-                ]),
-                ("hist", (img) -> (rand(UInt8, size(input)...), img), [
-                    (method = "opencv", fn = hist_opencv),
-                    (method = "finch", fn = hist_finch),
-                    (method = "finch_rle", fn = hist_finch_rle),
-                ]),
-                #=
-                ("histblur", [
-                    (method = "opencv", fn = histblur_opencv(rand_data)),
-                    (method = "finch", fn = histblur_finch(rand_data)),
-                    (method = "finch_rle", fn = histblur_finch_rle(rand_data)),
-                ]),
-                ("blur", [
-                    (method = "opencv", fn = blur_opencv(rand_data)),
-                    (method = "finch", fn = blur_finch(rand_data)),
-                    (method = "finch_rle", fn = blur_finch_rle(rand_data)),
-                ]),
-                =#
-            ]
-                input2 = prep(input)
+sample(r) = r[randperm(end)[1:min(N, end)]]
 
-                reference = nothing
+mnist_i = sample(1:mnist_train_length())
+omniglot_i = sample(1:omniglot_train_length())
+humansketches_i = sample(1:humansketches_length())
+testimage_dip3e_i = sample(dip3e_masks)
 
-                for kernel in kernels
-                    result = kernel.fn(input2)
+datas = Dict(
+    "mnist" => (mnist_train, mnist_i, (img) -> Array{UInt8}(img .> 0x02)),
+    "omniglot" => (omniglot_train, omniglot_i, (img) -> Array{UInt8}(img .== 0x00)),
+    "humansketches" => (humansketches, humansketches_i, (img) -> Array{UInt8}(reinterpret(UInt8, img) .< 0xF0)),
+    "testimage_dip3e" => (testimage_dip3e, testimage_dip3e_i, (img) -> Array{UInt8}(Array{Gray}(img) .> 0.1)),
+    "mnist_magnify" => (mnist_train, mnist_i, (img) -> Array{UInt8}(magnify(img, MAG_FACTOR) .> 0x02)),
+    "omniglot_magnify" => (omniglot_train, omniglot_i, (img) -> Array{UInt8}(magnify(img, MAG_FACTOR) .== 0x00)),
+    "humansketches_magnify" => (humansketches, humansketches_i, (img) -> Array{UInt8}(magnify(reinterpret(UInt8, img), MAG_FACTOR) .< 0xF0)),
+    "testimage_dip3e_magnify" => (testimage_dip3e, testimage_dip3e_i, (img) -> Array{UInt8}(magnify(Array{Gray}(img), MAG_FACTOR) .> 0.1)),
+)
 
-                    if reference == nothing && op in ["fill", "erode4"]
-                        Images.save("output/$(dataset)_$(op)_$(i).png", Array{Gray}(Array(result.output)))
-                    end
-                    reference = something(reference, result.output)
-                    if reference != result.output
-                        Images.save("output/$(dataset)_$(op)_$(i)_$(kernel.method).png", Array{Gray}(Array(result.output)))
-                    end
-                    @assert reference == result.output
+groups = Dict(
+    "all" => ["mnist", "omniglot", "humansketches", "testimage_dip3e", "mnist_magnify", "omniglot_magnify", "humansketches_magnify", "testimage_dip3e_magnify"],
+    "standard" => ["mnist", "omniglot", "humansketches", "testimage_dip3e"],
+    "magnify" => ["mnist_magnify", "omniglot_magnify", "humansketches_magnify", "testimage_dip3e_magnify"],
+    "mnist" => "mnist",
+    "omniglot" => "omniglot",
+    "humansketches" => "humansketches",
+    "testimage_dip3e" => "testimage_dip3e",
+    "mnist_magnify" => "mnist_magnify",
+    "omniglot_magnify" => "omniglot_magnify",
+    "humansketches_magnify" => "humansketches_magnify",
+    "testimage_dip3e_magnify" => "testimage_dip3e_magnify",
+)
 
-                    println("$op, $dataset [$i]: $(kernel.method) time: ", result.time, "\tmem: ", result.mem, "\tnnz: ", result.nnz)
+for dataset in groups[parsed_args["dataset"]]
+    (getdata, I, f) = datas[dataset]
+    for i in I
+        input = f(getdata(i))
 
-                    push!(results, Dict("operation" => op, "dataset"=>dataset, "label" => i, "method"=> kernel.method, "mem" => result.mem, "nnz" => result.nnz, "time"=>result.time))
-                    write(resultfile, JSON.json(results, 4))
+        for (op, prep, kernels) in [
+            ("fill", prep_fill, [
+                (method = "opencv", fn = fill_opencv),
+                (method = "finch", fn = fill_finch),
+                (method = "finch_scatter", fn = fill_finch_scatter),
+            ]),
+            ("erode2", (img) -> (img, 2), [
+                (method = "opencv", fn = erode_opencv),
+                (method = "finch", fn = erode_finch),
+                (method = "finch_rle", fn = erode_finch_rle),
+                (method = "finch_bits", fn = erode_finch_bits),
+                (method = "finch_bits_mask", fn = erode_finch_bits_mask),
+            ]),
+            ("erode4", (img) -> (img, 4), [
+                (method = "opencv", fn = erode_opencv),
+                (method = "finch", fn = erode_finch),
+                (method = "finch_rle", fn = erode_finch_rle),
+                (method = "finch_bits", fn = erode_finch_bits),
+                (method = "finch_bits_mask", fn = erode_finch_bits_mask),
+            ]),
+            ("hist", (img) -> (rand(UInt8, size(input)...), img), [
+                (method = "opencv", fn = hist_opencv),
+                (method = "finch", fn = hist_finch),
+                (method = "finch_rle", fn = hist_finch_rle),
+            ]),
+            #=
+            ("histblur", [
+                (method = "opencv", fn = histblur_opencv(rand_data)),
+                (method = "finch", fn = histblur_finch(rand_data)),
+                (method = "finch_rle", fn = histblur_finch_rle(rand_data)),
+            ]),
+            ("blur", [
+                (method = "opencv", fn = blur_opencv(rand_data)),
+                (method = "finch", fn = blur_finch(rand_data)),
+                (method = "finch_rle", fn = blur_finch_rle(rand_data)),
+            ]),
+            =#
+        ]
+            input2 = prep(input)
+
+            reference = nothing
+
+            for kernel in kernels
+                result = kernel.fn(input2)
+
+                #if reference == nothing && op in ["fill", "erode4"]
+                #    Images.save("output/$(dataset)_$(op)_$(i).png", Array{Gray}(Array(result.output)))
+                #end
+                reference = something(reference, result.output)
+                if reference != result.output
+                    Images.save("output/$(dataset)_$(op)_$(i)_$(kernel.method).png", Array{Gray}(Array(result.output)))
                 end
+                @assert reference == result.output
+
+                println("$op, $dataset [$i]: $(kernel.method) time: ", result.time, "\tmem: ", result.mem, "\tnnz: ", result.nnz)
+
+                push!(results, Dict("operation" => op, "dataset"=>dataset, "label" => i, "method"=> kernel.method, "mem" => result.mem, "nnz" => result.nnz, "time"=>result.time))
+                write(parsed_args["output"], JSON.json(results, 4))
             end
         end
     end
-
-    return results
 end
-
-main("test.json")
---
+++ morphology/run_morphology.sl
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --tasks-per-node=1
+#SBATCH -N 1
+#SBATCH --exclusive
+#SBATCH -t 04:00:00
+#SBATCH --partition=lanka-v3
+#SBATCH --qos=commit-main
+#SBATCH --mem 102400
+#SBATCH --array=1-8
+
+cd /data/scratch/willow/FinchBenchmarks/graphs
+source /afs/csail.mit.edu/u/w/willow/everyone/.bashrc
+
+echo $SCRATCH
+echo $JULIA_DEPOT_PATH
+echo $JULIAUP_DEPOT_PATH
+echo $PATH
+echo $(pwd)
+
+# Define an array of datasets
+DATASETS=("mnist" "omniglot" "humansketches" "testimage_dip3e" "mnist_magnify" "omniglot_magnify" "humansketches_magnify" "testimage_dip3e_magnify")
+# Get the corresponding dataset for this job index
+DATASET=${DATASETS[$SLURM_ARRAY_TASK_ID - 1]}
+
+julia run_morphology.jl --dataset $DATASET --output morphology_results_$SLURM_ARRAY_TASK_ID.json --num_trials 100
